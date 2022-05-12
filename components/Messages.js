c8bd8d5ba5408b59ff906f3a5bf0f9b61e4a697f
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2022-05-12 23:01:07 +0700

    added chat

--- components/Messages.js
+++ components/Messages.js
@@ -1,5 +1,29 @@
+import { useRef } from "react";
+import { ByMoralis, useMoralis } from "react-moralis";
+import SendMessage from "./SendMessage";
+
 function Messages() {
-  return <div>Messages</div>;
+  const { user } = useMoralis();
+  const endOfMessagesRef = useRef(null);
+  return (
+    <div>
+      <div className="my-5">
+        <ByMoralis
+          variant="dark"
+          style={{ marginLeft: "auto", marginRight: "auto" }}
+        />
+      </div>
+      <div>{/* Each Message */}</div>
+
+      <div className="flex justify-center">
+        <SendMessage endOfMessagesRef={endOfMessagesRef} />
+      </div>
+
+      <div ref={endOfMessagesRef} className="text-center text-gray-400 mt-5">
+        <p>You're up to date {user.getUsername()}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Messages;
