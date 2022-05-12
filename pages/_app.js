18c8e1ea1e75f8457249376039ce62889ca7e3bc
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2022-05-12 21:44:44 +0700

    added moralis

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
